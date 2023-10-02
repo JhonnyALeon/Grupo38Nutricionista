4366cbc85f160c3fa1f2dad8611f859523c9f8ce
Jhonny Leon <jhonnyaleon@gmail.com>
2023-10-02 17:19:29 -0300

    holamundo

--- src/grupo38nutricionista/Grupo38Nutricionista.java
+++ src/grupo38nutricionista/Grupo38Nutricionista.java
@@ -16,6 +16,9 @@ public class Grupo38Nutricionista {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //holamundo
+        
     }
     
 }
